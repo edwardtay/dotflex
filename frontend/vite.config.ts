cd89c337a3cfb4b1fcb86c345e510b83a9591f86
edwardtay <edwardtay7@gmail.com>
2025-11-15 15:50:55 +0800

    fix: configure Vite to handle bn.js CommonJS module properly
    - Add vite-plugin-node-polyfills for CommonJS support
    - Configure optimizeDeps to include Polkadot dependencies
    - Add global polyfill definition
    - Clear Vite cache to force rebuild

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer', 'process', 'util']
+    })
+  ],
   server: {
     port: 3000,
     host: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/node_modules/]
+    }
   },
   optimizeDeps: {
-    exclude: ['@polkadot/api', '@polkadot/util']
+    esbuildOptions: {
+      target: 'es2020'
+    },
+    include: [
+      'bn.js',
+      '@polkadot/api',
+      '@polkadot/util',
+      '@polkadot/util-crypto'
+    ]
+  },
+  define: {
+    global: 'globalThis'
   }
 })
 
